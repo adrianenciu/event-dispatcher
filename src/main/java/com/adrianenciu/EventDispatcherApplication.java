2bfb1465bffa66254289628ebe4f95ca6e0a0634
Adrian Enciu <adrian.enciu@1and1.ro>
2016-04-01 18:55:19 +0300

    - code formatting

--- src/main/java/com/adrianenciu/EventDispatcherApplication.java
+++ src/main/java/com/adrianenciu/EventDispatcherApplication.java
@@ -7,8 +7,8 @@ import org.springframework.jms.annotation.EnableJms;
 @SpringBootApplication
 @EnableJms
 public class EventDispatcherApplication {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
-		SpringApplication.run(EventDispatcherApplication.class, args);
-	}
+        SpringApplication.run(EventDispatcherApplication.class, args);
+    }
 }
